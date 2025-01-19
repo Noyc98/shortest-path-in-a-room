c22afa0f8a2a00242e1836076d8d0a5333ebb5e9
Almog Hadad <nulyc7@gmail.com>
2025-01-19 09:15:43 +0200

    Switched to dijkstra

--- visualization/main.py
+++ visualization/main.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     manager.show_map(with_dash=False, include_poly_dots=True, include_polygons=True)
 
     # C++ execution
-    manager.create_cpp_input(algorithm_type='A*') # Options: 'A*', 'Dijkstra', 'RRT'
+    manager.create_cpp_input(algorithm_type='Dijkstra') # Options: 'A*', 'Dijkstra', 'RRT'
     manager.find_path()
     manager.read_cpp_output()
 
-    # Plot output (visibility graph + path)
+    # Plot output
     manager.show_map(with_dash=False, include_poly_dots=False, include_polygons=True, include_path=True)
